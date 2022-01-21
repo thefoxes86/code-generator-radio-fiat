2d88906a777929a7191ca041c2990afe82c8e93a
thefoxes86 <nicola.volpi86@gmail.com>
2022-01-21 17:11:30 +0100

    Remove useRef

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useRef, useState } from "react";
+import { useState } from "react";
 
 function App() {
   let arrayResult = new Array(4);
